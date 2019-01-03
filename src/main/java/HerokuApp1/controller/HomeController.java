a91b79b6eb90d35be61af74cf0562a88815fcc3f
Feiron <rm2rm@mail.ru>
2019-01-03 17:38:30 +0200

    first commit

---
+++ src/main/java/HerokuApp1/controller/HomeController.java
@@ -0,0 +1,20 @@
+package HerokuApp1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping(value="/")
+@Controller
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("massge", "Hello Ruslan");
+		return "home";
+	}
+//	public ModelAndView test(HttpServletResponse response) throws IOException{
+//		return new ModelAndView("home");
+//	}
+}
